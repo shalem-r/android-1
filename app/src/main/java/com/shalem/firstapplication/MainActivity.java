ce4df33968fbd24b64dbcc57d21bf2291c94f042
Kadamanchi <shalemraju.kadamanchi@here.com>
2020-07-23 16:18:43 +0530

    New Files

---
+++ app/src/main/java/com/shalem/firstapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.shalem.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void sendNow(View view){
+        Toast.makeText(this,"Sending data from app...",Toast.LENGTH_SHORT).show();
+    }
+    public void receiveNow(View view){
+        Toast.makeText(this,"Receiving data...",Toast.LENGTH_SHORT).show();
+    }
+    public void deleteNow(View view){
+        Toast.makeText(this,"Deleting data from app...",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
